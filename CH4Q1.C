19ca9ca45f3c459fcaa79aa449a29290c1298476
ketan-bhunganii <ketanbhungani1933@gmail.com>
2024-08-02 11:17:57 +0530

    c-files

---
+++ CH4Q1.C
@@ -0,0 +1,19 @@
+#include<conio.h>
+#include<stdio.h>
+main()
+{
+	int x;
+	int y;
+	clrscr();
+	printf("enter the value x and y|");
+	scanf("%d%d",&x,&y);
+	printf("ans the value%d",(x+y)*(x+y));
+	getch();
+
+
+
+
+
+
+
+}
---
+++ CH4Q2.C
@@ -0,0 +1,11 @@
+#include<conio.h>
+#include<stdio.h>
+main()
+{
+
+
+
+
+
+
+}
